95bf49eb1037b196f11701d6ffd31cbf029cb88f
allentsangdev <allentsang04@gmail.com>
2023-01-24 14:33:13 -0500

    adding save tab function for chrome extention

--- Proj5_Chrome_Extention/index.js
+++ Proj5_Chrome_Extention/index.js
@@ -4,6 +4,7 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
 if(leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
@@ -35,4 +36,12 @@ deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
     render(myLeads)
+})
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)  
+     })
 })
